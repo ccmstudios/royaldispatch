d77bf4ffc8d92093c0688b2155c6172beb0cca2f
N.M. Porterfield <ccmstudiosinc@gmail.com>
2021-07-25 16:30:55 -0400

    navbar and logo

---
+++ royaldispatch/src/components/navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "../royaltylogo.png";
+const Navbar = () => {
+  return (
+    <div>
+      <ul id='navbar'>
+        <li>
+          <img src={logo} className="App-logo" alt="logo" />
+        </li>
+        <li>
+          <a href ='#'>Home</a>
+        </li>
+        <li>
+          <a href ='#'>About</a>
+        </li>
+        <li>
+          <a href ='#'>Services</a>
+        </li>
+        <li>
+          <a href ='#'>Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
