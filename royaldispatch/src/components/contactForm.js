a5509a33d681e6153d5390c926b690094655f958
N.M. Porterfield <ccmstudiosinc@gmail.com>
2021-08-13 01:24:48 -0400

    services

--- royaldispatch/src/components/contactForm.js
+++ royaldispatch/src/components/contactForm.js
@@ -10,26 +10,27 @@ class Contactform extends React.Component {
       <div id="form">
         <h1>Contact Us</h1>
         <Form id='contactform'>
-          <Form.Group className="mb-3" controlId="formGroupPassword">
-            <Form.Label>First Name</Form.Label>
+          <Form.Group className="mb-3" controlId="formGroupPassword" id='label'>
+            <Form.Label id='titles'>First Name</Form.Label>
             <Form.Control type="text" placeholder="First Name" />
-            <Form.Label>Last Name</Form.Label>
+            <Form.Label id='titles'>Last Name</Form.Label>
             <Form.Control type="text" placeholder="Last Name" />
-            <Form.Label>Phone Number</Form.Label>
+            <Form.Label id='titles'>Phone Number</Form.Label>
             <Form.Control type="text" placeholder="(555)555-5555" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formGroupEmail">
-            <Form.Label>Email address</Form.Label>
+          <Form.Group className="mb-3" controlId="formGroupEmail"id='label'>
+            <Form.Label id='titles'>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
             <Form.Group
               className="mb-3"
               controlId="exampleForm.ControlTextarea1"
+              id='label'
             >
-              <Form.Label>Comments</Form.Label>
+              <Form.Label id='titles'>Comments</Form.Label>
               <Form.Control as="textarea" rows={3} />
             </Form.Group>
             <br></br>
-            <Button variant="outline-warning">Submit</Button>{" "}
+            <Button variant="outline-dark">Submit</Button>{" "}
           </Form.Group>
         </Form>
       </div>
--- royaldispatch/src/pages/services/services.js
+++ royaldispatch/src/pages/services/services.js
@@ -2,90 +2,81 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
 import logo from "../../assets/images/royaltylogo.png";
+import "./services.css";
 
 class Services extends React.Component {
   render() {
     return (
-      <div id="services">
-        <img src={logo} className="App-logo" alt="logo" />
-        <CardGroup id="servicecards">
-          <Card bg="dark" border='warning'>
-            <Card.Img variant="top" src={logo} className="App-logo" />
-            <Card.Body id="cardbody">
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
-                et metus augue. Vivamus risus eros, iaculis eget ex id,
-                pellentesque aliquam nulla. Praesent accumsan eleifend lacus non
-                elementum. Phasellus iaculis magna sed lacinia gravida. Fusce
-                lacus libero, commodo sit amet felis in, vestibulum laoreet
-                orci. Maecenas cursus, tortor aliquet consectetur varius, enim
-                massa facilisis ligula, id elementum nulla turpis id purus.
-                Quisque vehicula odio vitae consequat malesuada. Cras sapien
-                risus, porta ut metus quis, sollicitudin porta libero. Etiam
-                hendrerit sapien felis, tincidunt commodo arcu sodales non.
-                Aenean efficitur sodales risus, quis viverra velit eleifend vel.
-                Praesent tincidunt dictum consectetur. Quisque fringilla
-                eleifend mi sit amet ornare. Mauris luctus ipsum quis ante
-                tempor tristique. Duis fringilla mattis nulla id efficitur.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card bg="dark" border='warning'>
-            <Card.Img variant="top" src={logo} className="App-logo" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
-                et metus augue. Vivamus risus eros, iaculis eget ex id,
-                pellentesque aliquam nulla. Praesent accumsan eleifend lacus non
-                elementum. Phasellus iaculis magna sed lacinia gravida. Fusce
-                lacus libero, commodo sit amet felis in, vestibulum laoreet
-                orci. Maecenas cursus, tortor aliquet consectetur varius, enim
-                massa facilisis ligula, id elementum nulla turpis id purus.
-                Quisque vehicula odio vitae consequat malesuada. Cras sapien
-                risus, porta ut metus quis, sollicitudin porta libero. Etiam
-                hendrerit sapien felis, tincidunt commodo arcu sodales non.
-                Aenean efficitur sodales risus, quis viverra velit eleifend vel.
-                Praesent tincidunt dictum consectetur. Quisque fringilla
-                eleifend mi sit amet ornare. Mauris luctus ipsum quis ante
-                tempor tristique. Duis fringilla mattis nulla id efficitur.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card bg="dark" border='warning'>
-            <Card.Img variant="top" src={logo} className="App-logo" />
-            <Card.Body id="cardbody">
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
-                et metus augue. Vivamus risus eros, iaculis eget ex id,
-                pellentesque aliquam nulla. Praesent accumsan eleifend lacus non
-                elementum. Phasellus iaculis magna sed lacinia gravida. Fusce
-                lacus libero, commodo sit amet felis in, vestibulum laoreet
-                orci. Maecenas cursus, tortor aliquet consectetur varius, enim
-                massa facilisis ligula, id elementum nulla turpis id purus.
-                Quisque vehicula odio vitae consequat malesuada. Cras sapien
-                risus, porta ut metus quis, sollicitudin porta libero. Etiam
-                hendrerit sapien felis, tincidunt commodo arcu sodales non.
-                Aenean efficitur sodales risus, quis viverra velit eleifend vel.
-                Praesent tincidunt dictum consectetur. Quisque fringilla
-                eleifend mi sit amet ornare. Mauris luctus ipsum quis ante
-                tempor tristique. Duis fringilla mattis nulla id efficitur.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-        </CardGroup>
-      </div>
+        <div className="container" id="services">
+          <div className="box">
+            <h2>01</h2>
+            <h3>Service One</h3>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
+              metus augue. Vivamus risus eros, iaculis eget ex id, pellentesque
+              aliquam nulla. Praesent accumsan eleifend lacus non elementum.
+              Phasellus iaculis magna sed lacinia gravida. Fusce lacus libero,
+              commodo sit amet felis in, vestibulum laoreet orci. Maecenas
+              cursus, tortor aliquet consectetur varius, enim massa facilisis
+              ligula, id elementum nulla turpis id purus. Quisque vehicula odio
+              vitae consequat malesuada. Cras sapien risus, porta ut metus quis,
+              sollicitudin porta libero. Etiam hendrerit sapien felis, tincidunt
+              commodo arcu sodales non. Aenean efficitur sodales risus, quis
+              viverra velit eleifend vel. Praesent tincidunt dictum consectetur.
+              Quisque fringilla eleifend mi sit amet ornare. Mauris luctus ipsum
+              quis ante tempor tristique. Duis fringilla mattis nulla id
+              efficitur.
+            </p>
+          </div>
+          <div className="box">
+            <h2>02</h2>
+            <h3>Service Two</h3>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
+              metus augue. Vivamus risus eros, iaculis eget ex id, pellentesque
+              aliquam nulla. Praesent accumsan eleifend lacus non elementum.
+              Phasellus iaculis magna sed lacinia gravida. Fusce lacus libero,
+              commodo sit amet felis in, vestibulum laoreet orci. Maecenas
+              cursus, tortor aliquet consectetur varius, enim massa facilisis
+              ligula, id elementum nulla turpis id purus. Quisque vehicula odio
+              vitae consequat malesuada. Cras sapien risus, porta ut metus quis,
+              sollicitudin porta libero. Etiam hendrerit sapien felis, tincidunt
+              commodo arcu sodales non. Aenean efficitur sodales risus, quis
+              viverra velit eleifend vel. Praesent tincidunt dictum consectetur.
+              Quisque fringilla eleifend mi sit amet ornare. Mauris luctus ipsum
+              quis ante tempor tristique. Duis fringilla mattis nulla id
+              efficitur.
+            </p>
+          </div>
+          <br></br>
+          <div className="box">
+            <h2>03</h2>
+            <h3>Service Three</h3>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
+              metus augue. Vivamus risus eros, iaculis eget ex id, pellentesque
+              aliquam nulla. Praesent accumsan eleifend lacus non elementum.
+              Phasellus iaculis magna sed lacinia gravida. Fusce lacus libero,
+              commodo sit amet felis in, vestibulum laoreet orci. Maecenas
+              cursus, tortor aliquet consectetur varius, enim massa facilisis
+              ligula, id elementum nulla turpis id purus. Quisque vehicula odio
+              vitae consequat malesuada. Cras sapien risus, porta ut metus quis,
+              sollicitudin porta libero. Etiam hendrerit sapien felis, tincidunt
+              commodo arcu sodales non. Aenean efficitur sodales risus, quis
+              viverra velit eleifend vel. Praesent tincidunt dictum consectetur.
+              Quisque fringilla eleifend mi sit amet ornare. Mauris luctus ipsum
+              quis ante tempor tristique. Duis fringilla mattis nulla id
+              efficitur.
+            </p>
+          </div>
+          {/* <div>
+
+<img src={logo} className="App-logo" alt="logo" />
+          </div> */}
+        </div>
     );
   }
 }
