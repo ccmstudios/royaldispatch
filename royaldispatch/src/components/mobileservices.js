47d86127d7f3b1302d4dc51b68cf0e36a5518a85
N.M. Porterfield <ccmstudiosinc@gmail.com>
2021-08-19 01:11:42 -0400

    media queries part one

--- royaldispatch/src/components/mobileservices.js
+++ royaldispatch/src/components/mobileservices.js
@@ -1,20 +1,38 @@
 import React from "react";
-import "./services.css";
-import Mobileservices from "../../components/mobileservices";
+import Card from "react-bootstrap/Card";
 
-
-class Services extends React.Component {
+class Mobileservices extends React.Component {
   render() {
     return (
-        <div id='mainServices'> 
-            <h1>Services</h1>
-            <Mobileservices/>
+      <div id="mobileservices">
+        <Card style={{width:'100%'}}>
+          <Card.Body>
+              <h1>Service One</h1>
+            <p id="mobileservicecard">
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
+              metus augue. Vivamus risus eros, iaculis eget ex id, pellentesque
+              aliquam nulla. Praesent accumsan eleifend lacus non elementum.
+              Phasellus iaculis magna sed lacinia gravida. Fusce lacus libero,
+              commodo sit amet felis in, vestibulum laoreet orci. Maecenas
+              cursus, tortor aliquet consectetur varius, enim massa facilisis
+              ligula, id elementum nulla turpis id purus. Quisque vehicula odio
+              vitae consequat malesuada. Cras sapien risus, porta ut metus quis,
+              sollicitudin porta libero. Etiam hendrerit sapien felis, tincidunt
+              commodo arcu sodales non. Aenean efficitur sodales risus, quis
+              viverra velit eleifend vel. Praesent tincidunt dictum consectetur.
+              Quisque fringilla eleifend mi sit amet ornare. Mauris luctus ipsum
+              quis ante tempor tristique. Duis fringilla mattis nulla id
+              efficitur.
+            </p>
+          </Card.Body>
+        </Card>
+        <br></br>
+        <Card>
+          <Card.Body>
+          <h1>Service Two</h1>
 
-        <div className="container" id="services">
-          <div className="box">
-            <h2>01</h2>
-            <h3>Service One</h3>
-            <p>
+            <p id="mobileservicecard">
               {" "}
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
               metus augue. Vivamus risus eros, iaculis eget ex id, pellentesque
@@ -31,11 +49,14 @@ class Services extends React.Component {
               quis ante tempor tristique. Duis fringilla mattis nulla id
               efficitur.
             </p>
-          </div>
-          <div className="box">
-            <h2>02</h2>
-            <h3>Service Two</h3>
-            <p>
+          </Card.Body>
+        </Card>
+        <br></br>
+        <Card>
+          <Card.Body>
+          <h1>Service Three</h1>
+
+            <p id="mobileservicecard">
               {" "}
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
               metus augue. Vivamus risus eros, iaculis eget ex id, pellentesque
@@ -52,34 +73,11 @@ class Services extends React.Component {
               quis ante tempor tristique. Duis fringilla mattis nulla id
               efficitur.
             </p>
-          </div>
-          <br></br>
-          <div className="box">
-            <h2>03</h2>
-            <h3>Service Three</h3>
-            <p>
-              {" "}
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et
-              metus augue. Vivamus risus eros, iaculis eget ex id, pellentesque
-              aliquam nulla. Praesent accumsan eleifend lacus non elementum.
-              Phasellus iaculis magna sed lacinia gravida. Fusce lacus libero,
-              commodo sit amet felis in, vestibulum laoreet orci. Maecenas
-              cursus, tortor aliquet consectetur varius, enim massa facilisis
-              ligula, id elementum nulla turpis id purus. Quisque vehicula odio
-              vitae consequat malesuada. Cras sapien risus, porta ut metus quis,
-              sollicitudin porta libero. Etiam hendrerit sapien felis, tincidunt
-              commodo arcu sodales non. Aenean efficitur sodales risus, quis
-              viverra velit eleifend vel. Praesent tincidunt dictum consectetur.
-              Quisque fringilla eleifend mi sit amet ornare. Mauris luctus ipsum
-              quis ante tempor tristique. Duis fringilla mattis nulla id
-              efficitur.
-            </p>
-          </div>
-  
-        </div>
-        </div>
+          </Card.Body>
+        </Card>
+      </div>
     );
   }
 }
 
-export default Services;
+export default Mobileservices;
