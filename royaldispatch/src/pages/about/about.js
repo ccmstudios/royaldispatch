8db3e519e18c96466b91f5fb3707ada19f7b16cc
N.M. Porterfield <ccmstudiosinc@gmail.com>
2021-09-14 20:17:53 -0400

    about me

--- royaldispatch/src/pages/about/about.js
+++ royaldispatch/src/pages/about/about.js
@@ -7,31 +7,32 @@ import "./about.css";
 class About extends React.Component {
   render() {
     return (
-      <div id='about'>
+      <div id="about">
         <Image id="aboutimage" src={harvey} roundedCircle />
         <div>
-            <h1>Meet Harvey Kimble</h1>
+          <h1>Meet Harvey Kimble</h1>
           <h3>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel
-            quam suscipit ante posuere tristique. Fusce non volutpat tellus, at
-            commodo nisl. Nunc massa dolor, mattis at accumsan sit amet, tempus
-            a erat. Donec et rhoncus sapien. Phasellus ut ligula massa. Cras
-            consequat metus sapien, nec luctus lacus cursus id. Fusce mollis
-            massa ante, vitae fermentum nisl gravida placerat. Nam accumsan
-            ullamcorper risus pulvinar tempor. Nam et urna quam. Etiam luctus
-            dui in risus blandit, ut pellentesque ex cursus. Nulla facilisi. Sed
-            tempus gravida nisi at finibus. Phasellus erat quam, semper id
-            vulputate eget, consequat sed leo. Sed viverra blandit auctor.
-            Praesent sagittis sagittis lacus sed tempor. Ut eu dignissim ante.
-            Duis a commodo ante. Integer rhoncus sit amet metus at vestibulum.
-            Nulla eget bibendum purus. Ut lacinia feugiat mi quis maximus. Cras
-            placerat arcu sit amet ante sagittis elementum. Fusce non mauris
-            consectetur, volutpat magna non, viverra turpis. Sed sit amet tortor
-            iaculis, rhoncus augue non, placerat metus. Donec at enim ac leo
-            mollis commodo. In euismod suscipit ex, eget blandit magna congue a.
-            Nam vitae libero lacinia, rutrum quam ut, posuere odio. Quisque
-            tempor justo vel nibh rhoncus volutpat. Vestibulum ante ipsum primis
-            in faucibus orci luctus et ultrices posuere cubilia curae;
+            Harvey Kimble is the Founder & Owner of Royalty Dispatching
+            Services. Starting out as a truck driver himself, he understands the
+            importance of organization, reliability & income. He also
+            understands that having hometime to spend with family is just as
+            important. Here at Royalty Dispatching, not only are the shippers
+            important the drivers are as well. Without us freight doesn't move.
+            This is why our main goal here at Royalty Dispatching is to provide
+            exceptional services for all of our clients. He began the idea after
+            being poorly dispatched. It was his daughter's birth that encouraged
+            him to start the business and learn more than just being a freight
+            mover. If no one else understands, he does because he has been out
+            on the road. Harvey has experienced the OTR life, just like you. He
+            has learned alot and is still learning how to make life on the road
+            easier & smoother for drivers. Royalty Dispatching provides services
+            to Owner Operators as well as Small Fleet Owners. If you are looking
+            for local or even dedicated lanes, we will do our very best to make
+            that happen. We insure that you will always receive the most
+            professional services with Royalty Dispatching Services! If you want
+            to work with someone as passionate as yourself, or even need someone
+            to take the load off of you, give us a call today & let's get you
+            rolling!
           </h3>
         </div>
         <img src={logo} className="App-logo" alt="logo" />
