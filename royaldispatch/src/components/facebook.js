d8bd37e2254aed273d0fcc13b75e483109ffa8a7
N.M. Porterfield <ccmstudiosinc@gmail.com>
2021-10-18 01:14:34 -0400

    messages added

--- royaldispatch/src/components/facebook.js
+++ royaldispatch/src/components/facebook.js
@@ -6,7 +6,7 @@ class Facebook extends React.Component {
       <div className="player-wrapper">
         <iframe
         title='facebook feed'
-          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Froyaltydispatchservicesllc&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=479993293087735"
+          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FRoyaltyDispatchServicesLLC&tabs=messages&width=340&height=500&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=false&appId=479993293087735"
           width="500"
           height="500"
           style={{border:"none",overflow:"hidden"}}
@@ -14,6 +14,7 @@ class Facebook extends React.Component {
           frameborder="0"
           allowfullscreen="true"
           allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+          
         ></iframe>
       </div>
     );
